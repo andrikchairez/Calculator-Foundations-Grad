cabb47e58073d9cf0e82df176c06b69d0af6b7e2
andrikchairez <132977393+andrikchairez@users.noreply.github.com>
2024-02-04 17:11:16 -0800

    Boilerplate files, w/basic operator logic for console testing

---
+++ javascript.js
@@ -0,0 +1,31 @@
+//Large steps
+//Create basic math operator functions
+//Your calculator is going to contain functions for all of the basic math operators you typically find on calculators, so start by creating functions for the following items and testing them in your browser’s console.
+// add
+// subtract
+// multiply
+// divide
+// A calculator operation will consist of a number, an operator, and another number. For example, 3 + 5. Create three variables for each of the parts of a calculator operation. Create a variable for the first number, the operator, and the second number. You’ll use these variables to update your display later.
+// Create a new function
+
+//digits to hold
+let digitOne;
+let digitTwo;
+let result = 0;
+
+function add(a, b){
+    return a + b;
+}
+
+function subtract(a,b){
+    return a - b;
+};
+
+function multiply(a,b){
+    return a * b;
+};
+
+function divide(a,b){
+    return a/b;
+};
+
